a9b0844799dbf12e950587cf647c375e4557cc4a
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-22 09:57:04 -0600

    first attempt at stubbing with mocha

--- test/controllers/tweet_stream_controller_test.rb
+++ test/controllers/tweet_stream_controller_test.rb
@@ -3,6 +3,11 @@ require "mocha/mini_test"
 
 class TweetStreamsControllerTest < ActionController::TestCase
   test "fetches tweets on create" do
+    jeff = Object.new
+    jeff.expects(:followers_count).returns(1)
+
+    @controller.twitter_client.expects(:user).with("j3").returns(jeff)
+
     post :create, :twitter_handle => "j3"
     assert_response :success
     assert_not_nil assigns(:tweets)
