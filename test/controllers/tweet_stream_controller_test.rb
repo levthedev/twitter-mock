4499dfd830fc26e017ecca55d48921a5a321bee7
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-22 10:33:13 -0600

    stubbing complete - 30x faster runtime

--- test/controllers/tweet_stream_controller_test.rb
+++ test/controllers/tweet_stream_controller_test.rb
@@ -5,6 +5,11 @@ class TweetStreamsControllerTest < ActionController::TestCase
   test "fetches tweets on create" do
     jeff = stub(followers_count: 1)
     @controller.twitter_client.expects(:user).with("j3").returns(jeff)
+    array = ["1", "2"]
+    me = "lev"
+    me.stubs(screen_name: "levthedev")
+    array.each { |string| string.stubs(user: me, text: "this is my tweet") }
+    @controller.twitter_client.expects(:user_timeline).with("j3").returns(array)
 
     post :create, :twitter_handle => "j3"
     assert_response :success
